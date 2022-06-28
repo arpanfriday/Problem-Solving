36295bf7bc5cdb8ac5e8111bad279d617cb0cbbd
Arpan Maity <arpanriju2000@gmail.com>
2022-06-28 15:06:31 +0530

    Tabulation added

--- DP/DiceGame.java
+++ DP/DiceGame.java
@@ -38,6 +38,19 @@ public class DiceGame {
         return count;
     }
 
+    static int countWaysTabu(int start, int end) {
+        int arr[] = new int[end + 1];
+        arr[end] = 1;
+        for (int i = end - 1; i >= 0; i--) {
+            int count = 0; // count number of ways
+            for (int dice = 1; dice <= 6 && dice + i < arr.length; dice++) {
+                count += arr[dice + i];
+            }
+            arr[i] = count;
+        }
+        return arr[0];
+    }
+
     public static void main(String[] args) {
         int start = 0;
         int end = 10;
@@ -46,6 +59,9 @@ public class DiceGame {
 
         int cache[] = new int[end + 1];
         count = countWaysMemo(start, end, cache);
+        System.out.println(count);
+
+        count = countWaysTabu(start, end);
         System.out.println(count);
     }
 }
