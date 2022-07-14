5621400dd80d1eafba16a8324b822c20a149d2ee
Arpan Maity <arpanriju2000@gmail.com>
2022-07-14 21:20:06 +0530

    Add files via upload

--- Tree/BinaryTreeDemo.java
+++ Tree/BinaryTreeDemo.java
@@ -1,5 +1,13 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
 import java.util.Scanner;
 import java.util.Stack;
+import java.util.TreeMap;
+
+import javax.management.Query;
 
 class BinaryTree<T> {
     T data;
@@ -31,9 +39,11 @@ class BinaryTreeOperations {
         msg = "left";
         node.left = insert();
         msg = "right";
+        parent = data;
         node.right = insert();
+        parent = data;
         msg = "root";
-        return node; // return root at the end
+        return node; // return root node
     }
 
     void print(BinaryTree<Integer> currentNode) {
@@ -64,7 +74,7 @@ class BinaryTreeOperations {
         preOrder(root.right);
     }
 
-    void preOrderIterative(BinaryTree<T> root) {
+    void preOrderIterative(BinaryTree<Integer> root) {
         if (root == null) {
             return;
         }
@@ -123,12 +133,125 @@ class BinaryTreeOperations {
         postOrder(root.right);
         System.out.println(root.data);
     }
+
+    void levelOrder(BinaryTree<Integer> root) {
+        // root node goes in a queue
+        // step 1 = create a queue
+        Queue<BinaryTree> queue = new LinkedList<>();
+        queue.add(root);
+        // step 2- traverse queue till it is not empty
+        // 2.1- queue poll and get the node and then look for left and right child
+        // 2.2- add them in qieie
+        // repeat step 2
+        while (!queue.isEmpty()) {
+            BinaryTree<Integer> node = queue.poll();// we get the first element
+            System.out.print(node.data + " , ");
+            if (node.left != null)
+                queue.add(node.left);
+            if (node.right != null)
+                queue.add(node.right);
+
+        }
+        System.out.println();
+    }
+
+    int height(BinaryTree<Integer> root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftHeight = height(root.left);
+        int rightHeight = height(root.right);
+        return 1 + Math.max(leftHeight, rightHeight);
+    }
+
+    int countNodes(BinaryTree<Integer> root) {
+        // If no node(reach to null) treat it as 0
+        if (root == null)
+            return 0;
+        int counter = 1;
+        counter += countNodes(root.left);
+        counter += countNodes(root.right);
+        return counter;
+    }
+
+    int maxLevel = 0;
+
+    void printLeftView(BinaryTree<Integer> root, int level) {
+        if (root == null) {
+            return;
+        }
+        if (maxLevel < level) {
+            System.out.println(root.data);
+            maxLevel = level;
+        }
+        printLeftView(root.left, level + 1);
+        printLeftView(root.right, level + 1);
+    }
+
+    void printLeftView2(BinaryTree<Integer> root) {
+        // Perform in BFT (Level order)
+        if (root == null) {
+            return;
+        }
+        Queue<BinaryTree<Integer>> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int count = queue.size();
+            for (int i = 0; i < count; i++) {
+                BinaryTree<Integer> node = queue.poll();
+                if (i == 0) {
+                    System.out.println(node.data);
+                }
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+        }
+    }
+
+    void verticalOrderHelper(BinaryTree<Integer> root, int distance, TreeMap<Integer, ArrayList<Integer>> map) {
+        // termination case
+        if (root == null) {
+            return;
+        }
+
+        // stack build
+        if (map.get(distance) == null) {
+            // there is no such distance present (first time coming in map)
+            // hence create a fresh list
+            ArrayList<Integer> list = new ArrayList<>();
+            list.add(root.data);
+            map.put(distance, list);
+        } else {
+            // the key is already present in the map (fetch the key from the map)
+            ArrayList<Integer> list = map.get(distance);
+            list.add(root.data);
+            map.put(distance, list);
+        }
+        // small problem
+        verticalOrderHelper(root.left, distance - 1, map);
+        verticalOrderHelper(root.right, distance + 1, map);
+    }
+
+    void verticalOrder(BinaryTree<Integer> root) {
+        int distance = 0;
+        TreeMap<Integer, ArrayList<Integer>> map = new TreeMap<>();
+        verticalOrderHelper(root, distance, map);
+        for (Map.Entry<Integer, ArrayList<Integer>> m : map.entrySet()) {
+            System.out.println(m.getKey() + " " + m.getValue());
+        }
+    }
 }
 
 public class BinaryTreeDemo {
     public static void main(String[] args) {
         BinaryTreeOperations opr = new BinaryTreeOperations();
         BinaryTree<Integer> root = opr.insert();
-        opr.print(root);
+        // opr.print(root);
+        // opr.levelOrder(root);
+        opr.verticalOrder(root);
     }
 }
