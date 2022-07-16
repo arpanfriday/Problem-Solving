b7ea2841b5c197843b420048c53e04d5a7919351
Arpan Maity <arpanriju2000@gmail.com>
2022-07-16 20:43:56 +0530

    Create height.java

---
+++ Tree/height.java
@@ -0,0 +1,8 @@
+int height(BinaryTree<Integer> root) {
+    if (root == null) {
+        return 0;
+    }
+    int leftHeight = height(root.left);
+    int rightHeight = height(root.right);
+    return 1 + Math.max(leftHeight, rightHeight);
+}
